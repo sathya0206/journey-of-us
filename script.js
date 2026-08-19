9520f5c161f7dc4881c4dcff6118619e94bc4927
sathya0206 <83503114+sathya0206@users.noreply.github.com>
2026-08-19 20:12:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -47,52 +47,52 @@ const questions = [
         question: "What was your first impression of me, and how wrong were you? 😂",
         answer: `1st impression i got (because of others) was you are a kind terror person who will use compass to prick if you don't like that person but actually you are kind and innocent girl.`
     },
-    // {
-    //     question: "What is one moment from these four years when you felt really proud of us?",
-    //     answer: `For me, when ever i say our love story to someone and they get amazed. 
-    //     Keeping that love strong and we become together forever. So our marriage itself a proud and happy moment for me.`
-    // },
+    {
+        question: "What is one moment from these four years when you felt really proud of us?",
+        answer: `For me, when ever i say our love story to someone and they get amazed. 
+        Keeping that love strong and we become together forever. So our marriage itself a proud and happy moment for me.`
+    },
 
-    // {
-    //     question: "What’s one small thing I do that makes you feel cared for, even if I don’t realise I’m doing it?",
-    //     answer: `Always you come and say, spend some money for yourself nothing wrong in that hit me differently`
-    // },
-    // {
-    //     question: "What is one moment with me that still makes you laugh whenever you remember it?",
-    //     answer: `There are many things, i couldn't remember now but when ever we talk even for silly things we laugh thinking of that gives me smile in my face now`
-    // },
-    // {
-    //     question: "Be honest… what’s the one thing I do that annoys you the most?",
-    //     answer: `You underestimate yourself and the fear you have`
-    // },
-    // {
-    //     question: "What do you think has changed the most about us since we got married?",
-    //     answer: `We become responsible for each other and our family. I miss that childish talks we use to have before our marriage.`
-    // },
-    // {
-    //     question: "When did it really hit you that we had gone from being just the two of us to becoming a family of three?",
-    //     answer: `Actually it took sometime to accept what was happening around us, but when you become normal is when i accepted we are a family of three.`
-    // },
-    // {
-    //     question: "What’s one thing you wish you had done more of during our first four years together?",
-    //     answer: `I wish we had gone for more trips , just the "us" time and some fun in trips you know what i mean.`
-    // },
-    // {
-    //     question: "What’s one of my habits that you pretend to be annoyed about but would actually miss if I stopped doing it?",
-    //     answer: `It's easy, you would call out my name for everything sometime it annoys me but i cannot live without that.`
-    // },
-    // {
-    //     question: "If you had to describe our four years together using only three words, what would they be?",
-    //     answer: `Definitely three words is not enough to describe our four years together. But if i had to use three words, it would be "Love, trust and care".`
-    // },
-    // {
-    //     question: "Who do you think has done more silly things in these four years - me or you? honest answer only",
-    //     answer: `I do more crazy things, you do silly things`
-    // },
-    // {
-    //     question: "What is one thing about our life together that you wouldn't want to change?",
-    //     answer: `I want this love, care and trust to continue forever along with the intimacy we have between us`
-    // }
+    {
+        question: "What’s one small thing I do that makes you feel cared for, even if I don’t realise I’m doing it?",
+        answer: `Always you come and say, spend some money for yourself nothing wrong in that hit me differently`
+    },
+    {
+        question: "What is one moment with me that still makes you laugh whenever you remember it?",
+        answer: `There are many things, i couldn't remember now but when ever we talk even for silly things we laugh thinking of that gives me smile in my face now`
+    },
+    {
+        question: "Be honest… what’s the one thing I do that annoys you the most?",
+        answer: `You underestimate yourself and the fear you have`
+    },
+    {
+        question: "What do you think has changed the most about us since we got married?",
+        answer: `We become responsible for each other and our family. I miss that childish talks we use to have before our marriage.`
+    },
+    {
+        question: "When did it really hit you that we had gone from being just the two of us to becoming a family of three?",
+        answer: `Actually it took sometime to accept what was happening around us, but when you become normal is when i accepted we are a family of three.`
+    },
+    {
+        question: "What’s one thing you wish you had done more of during our first four years together?",
+        answer: `I wish we had gone for more trips , just the "us" time and some fun in trips you know what i mean.`
+    },
+    {
+        question: "What’s one of my habits that you pretend to be annoyed about but would actually miss if I stopped doing it?",
+        answer: `It's easy, you would call out my name for everything sometime it annoys me but i cannot live without that.`
+    },
+    {
+        question: "If you had to describe our four years together using only three words, what would they be?",
+        answer: `Definitely three words is not enough to describe our four years together. But if i had to use three words, it would be "Love, trust and care".`
+    },
+    {
+        question: "Who do you think has done more silly things in these four years - me or you? honest answer only",
+        answer: `I do more crazy things, you do silly things`
+    },
+    {
+        question: "What is one thing about our life together that you wouldn't want to change?",
+        answer: `I want this love, care and trust to continue forever along with the intimacy we have between us`
+    }
 
 ];
 
